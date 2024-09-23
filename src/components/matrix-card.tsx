233fc1af2110ca7c5b855ea51d3e80fe749d2da3
Pawel Gnat <pawel_gnat@o2.pl>
2024-09-23 18:41:21 +0200

    build: dnd kit

--- src/components/matrix-card.tsx
+++ src/components/matrix-card.tsx
@@ -8,6 +8,7 @@ export const MatrixCard = ({ title }: MatrixCardProps) => {
   return (
     <Card className="mx-auto flex w-full cursor-grab flex-col justify-between p-4 transition-transform hover:shadow-md active:scale-[1.01] active:cursor-grabbing active:shadow-lg">
       <CardTitle className="select-none truncate text-lg font-medium">{title}</CardTitle>
+      <span></span>
     </Card>
   );
 };
