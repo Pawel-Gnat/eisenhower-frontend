8c2db53615807769211956d8ea3d2bf2bc1b00e2
Pawel Gnat <pawel_gnat@o2.pl>
2024-11-27 11:00:39 +0100

    build: vitest, rtl

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import path, { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-})
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'],
+    setupFiles: [resolve(__dirname, './tests/setup.ts')],
+  },
+});
